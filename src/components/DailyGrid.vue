29000da52ab0df296f09ee53ee8287e9932d71d0
maxmetcalf12 <maxmetcalf12@gmail.com>
2023-10-16 23:06:15 -0600

    Added styling and data to grid

--- src/components/DailyGrid.vue
+++ src/components/DailyGrid.vue
@@ -1,12 +1,54 @@
 <script setup>
 // IMPORTS
+import { ref } from 'vue'
 
 // PROPS
 
 // STORES
 
 // DATA & REFS
-
+const pickems = ref([
+  [
+    {
+      team_name: 'Jazz',
+      team_logo: 'https://content.sportslogos.net/logos/6/234/full/utah_jazz_logo_primary_19973688.png'
+    },
+    {
+      team_name: 'Kings',
+      team_logo: 'https://content.sportslogos.net/logos/6/234/full/utah_jazz_logo_primary_19973688.png'
+    },
+  ],
+  [
+    {
+      team_name: 'Jazz',
+      team_logo: 'https://content.sportslogos.net/logos/6/234/full/utah_jazz_logo_primary_19973688.png'
+    },
+    {
+      team_name: 'Kings',
+      team_logo: 'https://content.sportslogos.net/logos/6/234/full/utah_jazz_logo_primary_19973688.png'
+    },
+  ],
+  [
+    {
+      team_name: 'Jazz',
+      team_logo: 'https://content.sportslogos.net/logos/6/234/full/utah_jazz_logo_primary_19973688.png'
+    },
+    {
+      team_name: 'Kings',
+      team_logo: 'https://content.sportslogos.net/logos/6/234/full/utah_jazz_logo_primary_19973688.png'
+    },
+  ],
+  [
+    {
+      team_name: 'Jazz',
+      team_logo: 'https://content.sportslogos.net/logos/6/234/full/utah_jazz_logo_primary_19973688.png'
+    },
+    {
+      team_name: 'Kings',
+      team_logo: 'https://content.sportslogos.net/logos/6/234/full/utah_jazz_logo_primary_19973688.png'
+    },
+  ],
+])
 // COMPUTED
 
 // EMITS
@@ -18,21 +60,17 @@
 
 <template>
   <div class="grid grid-cols-2 w-auto max-w-screen-lg gap-12">
-    <div class="h-64 border shadow divide-y">
-      <div class="h-1/2 flex justify-center items-center">Option 1</div>
-      <div class="h-1/2 flex justify-center items-center">Option 2</div>
-    </div>
-    <div class="h-64 border shadow divide-y">
-      <div class="h-1/2 flex justify-center items-center">Option 1</div>
-      <div class="h-1/2 flex justify-center items-center">Option 2</div>
-    </div>
-    <div class="h-64 border shadow divide-y">
-      <div class="h-1/2 flex justify-center items-center">Option 1</div>
-      <div class="h-1/2 flex justify-center items-center">Option 2</div>
-    </div>
-    <div class="h-64 border shadow divide-y">
-      <div class="h-1/2 flex justify-center items-center">Option 1</div>
-      <div class="h-1/2 flex justify-center items-center">Option 2</div>
+    <div v-for="(pickem, index) in pickems" :key="index" class="h-64 border shadow divide-y">
+      <div v-for="pick in pickem" :key=pick.team_name class="h-1/2 flex justify-center items-center cursor-pointer hover:bg-gray-100">
+        <div class="w-32 h-32 flex items-center">
+          <img
+            class="object-cover"
+            :src="pick.team_logo"
+            alt=""
+          >
+        </div>
+        <p class="text-2xl font-semibold">{{ pick.team_name }}</p>
+      </div>
     </div>
   </div>
 </template>
